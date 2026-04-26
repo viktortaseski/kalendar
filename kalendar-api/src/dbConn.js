dfcd9ee4606447437edc741432050ffac44e9afd
Viktor Taseski <vtaseski24@gmail.com>
2026-04-26 17:26:06 +0200

    dbConn bugfix

--- kalendar-api/src/dbConn.js
+++ kalendar-api/src/dbConn.js
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('.render.com')
+  ssl: process.env.DATABASE_URL?.includes('dpg-')
         ? { rejectUnauthorized: false }
         : false,
 })
