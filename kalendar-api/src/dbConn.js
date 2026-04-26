0b2d2a0419e9d9728683eb9c74af1b766d3efad9
Viktor Taseski <vtaseski24@gmail.com>
2026-04-26 14:39:22 +0200

    Add no-op build script for Render and changed to internal db url

--- kalendar-api/src/dbConn.js
+++ kalendar-api/src/dbConn.js
@@ -2,14 +2,10 @@ import 'dotenv/config';
 import { Pool } from 'pg';
 
 export const db = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_HOST?.includes('render.com') ? {
-    rejectUnauthorized: false,
-  } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('.render.com')
+        ? { rejectUnauthorized: false }
+        : false,
 })
 
 db.on('error', (err) => {
